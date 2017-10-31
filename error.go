53230f48b0cef94338bf4bab293ebaecf5e5e358
kefeng <pankefeng@asmartway.cn>
2017-10-31 18:44:31 +0800

    增加预定义错误模块，

--- error.go
+++ error.go
@@ -7,25 +7,33 @@ import (
 	"sync/atomic"
 )
 
-var debug int32= 0
+var debug int32 = 0
 
-func EnableDebug(){
-	atomic.StoreInt32(&debug,1)
+func EnableDebug() {
+	atomic.StoreInt32(&debug, 1)
 }
-func DisableDebug(){
-	atomic.StoreInt32(&debug,0)
+func DisableDebug() {
+	atomic.StoreInt32(&debug, 0)
 }
 
 //判断两错误是否同源相等
-func Equal(e1,e2 error)bool{
-	if e1 == e2 {return true}
-	if e1 == nil || e2 == nil{return false}
-	E1,ok1 :=e1.(*Error)
-	E2,ok2 :=e2.(*Error)
-	if ok2 && !ok1{return E2.rawErr == e1}
-	if ok1 && !ok2{return E1.rawErr == e2}
+func Equal(e1, e2 error) bool {
+	if e1 == e2 {
+		return true
+	}
+	if e1 == nil || e2 == nil {
+		return false
+	}
+	E1, ok1 := e1.(*Error)
+	E2, ok2 := e2.(*Error)
+	if ok2 && !ok1 {
+		return E2.rawErr == e1
+	}
+	if ok1 && !ok2 {
+		return E1.rawErr == e2
+	}
 
-	if E1.rawErr == E2.rawErr && e1!=nil{
+	if E1.rawErr == E2.rawErr && e1 != nil {
 		return true
 	}
 	if E1.Info == E2.Info && E1 == E2 {
@@ -39,18 +47,10 @@ type Errorer interface {
 }
 
 type Error struct {
-	Info string
-	rawErr error     // 保存原始错误信息
+	Code    int
+	Info    string
+	rawErr  error     // 保存原始错误信息
 	stackPC []uintptr // 保存函数调用栈指针
-}
-
-func (e *Error) MarshalJSON() ([]byte, error){
-	return []byte(fmt.Sprintf(`"%s"`,e.Info)),nil
-}
-
-func (e *Error) UnmarshalJSON(d []byte) error{
-	e.Info = string(d)
-	return nil
 }
 
 func (e *Error) IsNil() bool {
@@ -59,21 +59,23 @@ func (e *Error) IsNil() bool {
 func (e *Error) String() string {
 	return e.Info
 }
+
 //
 func (e *Error) MarkPos() *Error {
 	pcs := make([]uintptr, 32)
 	count := runtime.Callers(2, pcs)
 	return &Error{
-		Info:e.Info,
-		stackPC:pcs[:count],
-		rawErr:e.rawErr,
+		Info:    e.Info,
+		Code:    e.Code,
+		stackPC: pcs[:count],
+		rawErr:  e.rawErr,
 	}
 }
 
 // CallStack get function call stack
 func (e *Error) Error() string {
 
-	if atomic.LoadInt32(&debug) == 0{
+	if atomic.LoadInt32(&debug) == 0 {
 
 		return e.Info
 	}
@@ -90,7 +92,7 @@ func (e *Error) Error() string {
 			// trim GOPATH or GOROOT prifix
 			f.File = string(f.File[index+4:])
 		}
-		if f.Function == "runtime.main"{
+		if f.Function == "runtime.main" {
 			break
 		}
 		result = fmt.Sprintf("%s%s\n\t%s:%d\n", result, f.Function, f.File, f.Line)
@@ -98,17 +100,18 @@ func (e *Error) Error() string {
 			break
 		}
 	}
-	return fmt.Sprintf("%s\n%s",e.Info,result)
+	return fmt.Sprintf("%s\n%s", e.Info, result)
 }
 
-func New(s interface{})*Error{
+func newError(c int, s interface{}) *Error {
 	pcs := make([]uintptr, 32)
-	count := runtime.Callers(2, pcs)
-	e:=&Error{
-		Info:fmt.Sprint(s),
-		stackPC:pcs[:count],
+	count := runtime.Callers(3, pcs)
+	e := &Error{
+		Info:    fmt.Sprint(s),
+		Code:    c,
+		stackPC: pcs[:count],
 	}
-	switch raw:= s.(type){
+	switch raw := s.(type) {
 	case error:
 		e.rawErr = raw
 	default:
@@ -116,27 +119,50 @@ func New(s interface{})*Error{
 	}
 	return e
 }
+func New(s interface{}) *Error {
+	return newError(-1, s)
+}
 
+func NewWithCode(c int, s interface{}) *Error {
+	return newError(c, s)
+}
 
-func Errorf(format string, a ...interface{}) *Error {
+func NewCode(c int) *Error {
+	return &Error{Code: c}
+}
+func errorf(c int, format string, a ...interface{}) *Error {
 	pcs := make([]uintptr, 32)
-	count := runtime.Callers(2, pcs)
-	e:=&Error{
-		Info:fmt.Sprintf(format, a...),
-		stackPC:pcs[:count],
+	count := runtime.Callers(3, pcs)
+	e := &Error{
+		Info:    fmt.Sprintf(format, a...),
+		Code:    c,
+		stackPC: pcs[:count],
 	}
 	e.rawErr = e
 	return e
 }
+func ErrorfWithCode(c int, format string, a ...interface{}) *Error {
+	return errorf(c, format, a)
+}
+func Errorf(format string, a ...interface{}) *Error {
+	return errorf(-1, format, a)
+}
 
-func Errorln(a ...interface{}) *Error {
+func errorln(c int, a ...interface{}) *Error {
 	pcs := make([]uintptr, 32)
-	count := runtime.Callers(2, pcs)
+	count := runtime.Callers(3, pcs)
 	e := &Error{
-		Info:fmt.Sprintln(a...),
-		stackPC:pcs[:count],
+		Info:    fmt.Sprintln(a...),
+		Code:    c,
+		stackPC: pcs[:count],
 	}
 	e.rawErr = e
 	return e
+}
+func ErrorlnWithCode(c int, a ...interface{}) *Error {
+	return errorln(c, a)
+}
+func Errorln(a ...interface{}) *Error {
+	return errorln(-1, a)
 }
 
---
+++ predefine.go
@@ -0,0 +1,51 @@
+package errors
+
+import (
+	"runtime"
+	"fmt"
+)
+
+type PreDefineCode int
+
+func (e PreDefineCode) New(s interface{}) *Error {
+	pcs := make([]uintptr, 32)
+	count := runtime.Callers(2, pcs)
+	e1 := &Error{
+		Info:    fmt.Sprint(s),
+		Code:    int(e),
+		stackPC: pcs[:count],
+	}
+	switch raw := s.(type) {
+	case error:
+		e1.rawErr = raw
+	default:
+		e1.rawErr = e1
+	}
+	return e1
+}
+func (e PreDefineCode) Errorf(format string, a ...interface{}) *Error {
+	pcs := make([]uintptr, 32)
+	count := runtime.Callers(2, pcs)
+	e1 := &Error{
+		Info:    fmt.Sprintf(format, a...),
+		Code:    int(e),
+		stackPC: pcs[:count],
+	}
+	e1.rawErr = e1
+	return e1
+}
+
+func (e PreDefineCode) Errorln(a ...interface{}) *Error {
+	pcs := make([]uintptr, 32)
+	count := runtime.Callers(2, pcs)
+
+	e1 :=  &Error{
+		Info:    fmt.Sprintln(a...),
+		Code:    int(e),
+		stackPC: pcs[:count],
+	}
+	e1.rawErr = e1
+	return e1
+
+}
+
